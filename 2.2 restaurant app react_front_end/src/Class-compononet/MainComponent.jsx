f14dcf4d6d2486677c732c25eb000fbe4153d604
Tokuma Abdisa <106821330+tokukiyu@users.noreply.github.com>
2023-03-09 09:45:14 +0300

    react route

---
+++ 2.2 restaurant app react_front_end/src/Class-compononet/MainComponent.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import Menu from "./menuComponent";
+import DishDetail from "./DishdetailComponent";
+import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dishId) {
+    this.setState({ selectedDish: dishId });
+  }
+
+  render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+    return (
+      <div>
+        <Header />
+
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Redirect to="/home" />
+        </Switch>
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+        <Footer />
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === this.state.selectedDish
+            )[0]
+          }
+        />
+      </div>
+    );
+  }
+}
+
+export default Main;
