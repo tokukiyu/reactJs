93de96c80d664d49985d4b6408fb8db1f74ae380
tokukiyu <tokkiyuam@gmail.com>
2023-09-18 10:46:29 +0300

    update

--- metafinal/src/Calculator.jsx
+++ metafinal/src/Calculator.jsx
@@ -1,134 +1,87 @@
 import React, { Component } from 'react';
+import './Calculator.css';
 
 class Calculator extends Component {
   constructor() {
     super();
     this.state = {
-      displayValue: '0',
-      prevValue: null,
+      display: '0',
+      currentInput: '',
       operator: null,
-      waitingForOperand: false,
+      previousInput: '',
     };
   }
 
-  inputDigit = (digit) => {
-    const { displayValue, waitingForOperand } = this.state;
+  handleNumberClick = (num) => {
+    const { currentInput, display } = this.state;
+    if (currentInput.length < 9) {
+      const updatedInput = currentInput === '0' ? num : currentInput + num;
+      this.setState({ currentInput: updatedInput, display: updatedInput });
+    }
+  };
 
-    if (waitingForOperand) {
+  handleOperatorClick = (operator) => {
+    const { currentInput, previousInput } = this.state;
+    if (currentInput !== '') {
       this.setState({
-        displayValue: String(digit),
-        waitingForOperand: false,
-      });
-    } else {
-      this.setState({
-        displayValue:
-          displayValue === '0' ? String(digit) : displayValue + digit,
+        operator,
+        previousInput: currentInput,
+        currentInput: '',
       });
     }
   };
 
-  inputDecimal = () => {
-    const { displayValue, waitingForOperand } = this.state;
-
-    if (waitingForOperand) {
+  handleEqualsClick = () => {
+    const { currentInput, previousInput, operator } = this.state;
+    if (previousInput !== '' && currentInput !== '') {
+      const result = eval(`${previousInput} ${operator} ${currentInput}`);
       this.setState({
-        displayValue: '0.',
-        waitingForOperand: false,
-      });
-    } else if (displayValue.indexOf('.') === -1) {
-      this.setState({
-        displayValue: displayValue + '.',
+        display: result.toString().substring(0, 9),
+        currentInput: result.toString().substring(0, 9),
+        previousInput: '',
+        operator: null,
       });
     }
   };
 
-  clearDisplay = () => {
+  handleClearClick = () => {
     this.setState({
-      displayValue: '0',
-      prevValue: null,
+      display: '0',
+      currentInput: '',
       operator: null,
-      waitingForOperand: false,
+      previousInput: '',
     });
-  };
-
-  toggleSign = () => {
-    const { displayValue } = this.state;
-
-    this.setState({
-      displayValue: displayValue.charAt(0) === '-' ? displayValue.substr(1) : '-' + displayValue,
-    });
-  };
-
-  performOperation = (nextOperator) => {
-    const { displayValue, prevValue, operator } = this.state;
-    const inputValue = parseFloat(displayValue);
-
-    if (prevValue == null) {
-      this.setState({
-        prevValue: inputValue,
-        waitingForOperand: true,
-        operator: nextOperator,
-      });
-    } else if (operator) {
-      const currentValue = prevValue || 0;
-      const newValue = this.calculate(currentValue, inputValue, operator);
-
-      this.setState({
-        displayValue: String(newValue),
-        prevValue: newValue,
-        waitingForOperand: true,
-        operator: nextOperator,
-      });
-    }
-  };
-
-  calculate = (prevValue, nextValue, operator) => {
-    switch (operator) {
-      case '+':
-        return prevValue + nextValue;
-      case '-':
-        return prevValue - nextValue;
-      case '*':
-        return prevValue * nextValue;
-      case '/':
-        return prevValue / nextValue;
-      default:
-        return nextValue;
-    }
   };
 
   render() {
-    const { displayValue } = this.state;
-
+    const { display } = this.state;
     return (
       <div className="calculator">
-        <div className="display">{displayValue}</div>
+        <div className="display">{display}</div>
         <div className="buttons">
           <div className="row">
-            <button onClick={() => this.clearDisplay()}>AC</button>
-            <button onClick={() => this.toggleSign()}>+/-</button>
-            <button onClick={() => this.performOperation('/')}>/</button>
-            <button onClick={() => this.inputDigit(7)}>7</button>
-            <button onClick={() => this.inputDigit(8)}>8</button>
-            <button onClick={() => this.inputDigit(9)}>9</button>
-            <button onClick={() => this.performOperation('*')}>*</button>
+            <button onClick={() => this.handleNumberClick('7')}>7</button>
+            <button onClick={() => this.handleNumberClick('8')}>8</button>
+            <button onClick={() => this.handleNumberClick('9')}>9</button>
+            <button onClick={() => this.handleOperatorClick('+')}>+</button>
           </div>
           <div className="row">
-            <button onClick={() => this.inputDigit(4)}>4</button>
-            <button onClick={() => this.inputDigit(5)}>5</button>
-            <button onClick={() => this.inputDigit(6)}>6</button>
-            <button onClick={() => this.performOperation('-')}>-</button>
+            <button onClick={() => this.handleNumberClick('4')}>4</button>
+            <button onClick={() => this.handleNumberClick('5')}>5</button>
+            <button onClick={() => this.handleNumberClick('6')}>6</button>
+            <button onClick={() => this.handleOperatorClick('-')}>-</button>
           </div>
           <div className="row">
-            <button onClick={() => this.inputDigit(1)}>1</button>
-            <button onClick={() => this.inputDigit(2)}>2</button>
-            <button onClick={() => this.inputDigit(3)}>3</button>
-            <button onClick={() => this.performOperation('+')}>+</button>
+            <button onClick={() => this.handleNumberClick('1')}>1</button>
+            <button onClick={() => this.handleNumberClick('2')}>2</button>
+            <button onClick={() => this.handleNumberClick('3')}>3</button>
+            <button onClick={() => this.handleOperatorClick('*')}>*</button>
           </div>
           <div className="row">
-            <button onClick={() => this.inputDigit(0)}>0</button>
-            <button onClick={() => this.inputDecimal()}>.</button>
-            <button onClick={() => this.performOperation('=')}>=</button>
+            <button onClick={() => this.handleNumberClick('0')}>0</button>
+            <button onClick={this.handleClearClick}>C</button>
+            <button onClick={this.handleEqualsClick}>=</button>
+            <button onClick={() => this.handleOperatorClick('/')}>/</button>
           </div>
         </div>
       </div>
